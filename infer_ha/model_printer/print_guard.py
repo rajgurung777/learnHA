a967827fadd211abbe59981964c8c3dfb873e0e0
Amit Gurung <rajgurung777@gmail.com>
2023-04-27 12:02:38 +0900

    Normalized guard equation by dividing both sides by abs() of first coefficient

--- infer_ha/model_printer/print_guard.py
+++ infer_ha/model_printer/print_guard.py
@@ -14,10 +14,10 @@ def print_guard(f_out, coeffs, gExp):
     trans_guard = "guard:\n"
     f_out.write(trans_guard)
     guard = ""
-    coef1 = coeffs[0]  # first column coefficient, can be used to divide on both sides to normalize
+    coef1 = abs(coeffs[0])  # first column coefficient, can be used to divide on both sides to normalize. Taking only value not sign
     for i in range(0, len(coeffs)):  # for each term of the polynomial
         coef_val = coeffs[i]
-        if (coef1 != 0 and coef1 > 1):  # divide only when first coefficient is >1 to normalize and not spoil/raise values
+        if (coef1 != 0) and (coef1 > 1  or coef1 < 1):  # divide only when first coefficient is >1 to normalize and not spoil/raise values
             coef_val = coef_val / coef1  # Dividing all coefficient by the 1st coefficient, since right hand side of the equation is 0
             # coef_val = coef_val / 100  # guard coefficient further scaled down by 100 for avoiding large numerical calculation issues
 
